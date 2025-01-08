7c74d580fd47fde7bca9429a66d0f5689ffb780a
yogesh1q2w <yogesh120699@gmail.com>
2025-01-08 23:09:50 +0100

    profiling script

--- profile.sh
+++ profile.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 LOG_FILE="dopamine_dqn"
+GIN_FILE="dopamine/jax/agents/dqn/configs/dqn.gin"
 
-echo "launch train dqn-dopamine"
+echo launch train ${LOG_FILE}
 source env/bin/activate
 python3 dopamine/discrete_domains/train.py --base_dir ./logs/${LOG_FILE}\
- --gin_files dopamine/jax/agents/dqn/configs/dqn.gin\
+ --gin_files $GIN_FILE\
   &> ./logs/${LOG_FILE}.out &
 wait
