e523a7a673e8a5ed15dda34cf507151edf51b849
yogesh1q2w <yogesh120699@gmail.com>
2025-01-08 21:45:21 +0100

    black

--- dopamine/labs/atari_100k/train.py
+++ dopamine/labs/atari_100k/train.py
@@ -32,28 +32,25 @@ from dopamine.labs.atari_100k import spr_agent
 import numpy as np
 import tensorflow as tf
 
-.learning.deepmind.xmanager2.client.google as xm  # pylint: disable=unused-import
-
+# .learning.deepmind.xmanager2.client.google as xm  # pylint: disable=unused-import
 
 
 FLAGS = flags.FLAGS
-AGENTS = ['DER', 'DrQ', 'OTRainbow', 'DrQ_eps', 'SPR']
+AGENTS = ["DER", "DrQ", "OTRainbow", "DrQ_eps", "SPR"]
 
 # flags are defined when importing run_xm_preprocessing
-flags.DEFINE_enum('agent', 'DER', AGENTS, 'Name of the agent.')
-flags.DEFINE_integer('run_number', 1, 'Run number.')
+flags.DEFINE_enum("agent", "DER", AGENTS, "Name of the agent.")
+flags.DEFINE_integer("run_number", 1, "Run number.")
+flags.DEFINE_boolean("max_episode_eval", True, "Whether to use `MaxEpisodeEvalRunner` or not.")
 flags.DEFINE_boolean(
-    'max_episode_eval', True, 'Whether to use `MaxEpisodeEvalRunner` or not.'
-)
-flags.DEFINE_boolean(
-    'legacy_runner',
+    "legacy_runner",
     False,
     (
-        'Whether to use the legacy MaxEpisodeEvalRunner.'
-        ' This runner does not run parallel evaluation environments and may be'
-        ' easier to understand, but will be noticeably slower. It also does not'
-        ' guarantee that a precise number of training steps will be collected,'
-        ' which clashes with the technical definition of Atari 100k.'
+        "Whether to use the legacy MaxEpisodeEvalRunner."
+        " This runner does not run parallel evaluation environments and may be"
+        " easier to understand, but will be noticeably slower. It also does not"
+        " guarantee that a precise number of training steps will be collected,"
+        " which clashes with the technical definition of Atari 100k."
     ),
 )
 
@@ -65,57 +62,53 @@ def create_agent(
     agent_name: str,
     summary_writer=None,
 ):
-  """Helper function for creating full rainbow-based Atari 100k agent."""
+    """Helper function for creating full rainbow-based Atari 100k agent."""
 
-  if agent_name == 'SPR':
-    return spr_agent.SPRAgent(
+    if agent_name == "SPR":
+        return spr_agent.SPRAgent(
+            num_actions=environment.action_space.n,
+            seed=seed,
+            summary_writer=summary_writer,
+        )
+
+    return atari_100k_rainbow_agent.Atari100kRainbowAgent(
         num_actions=environment.action_space.n,
         seed=seed,
         summary_writer=summary_writer,
     )
 
-  return atari_100k_rainbow_agent.Atari100kRainbowAgent(
-      num_actions=environment.action_space.n,
-      seed=seed,
-      summary_writer=summary_writer,
-  )
-
 
 def set_random_seed(seed):
-  """Set random seed for reproducibility."""
-  logging.info('Setting random seed: %d', seed)
-  os.environ['PYTHONHASHSEED'] = str(seed)
-  tf.random.set_seed(seed)
-  np.random.seed(seed)
+    """Set random seed for reproducibility."""
+    logging.info("Setting random seed: %d", seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    tf.random.set_seed(seed)
+    np.random.seed(seed)
 
 
 def main(unused_argv):
-  """Main method.
+    """Main method.
 
-  Args:
-    unused_argv: Arguments (unused).
-  """
-  logging.set_verbosity(logging.INFO)
-  tf.compat.v1.enable_v2_behavior()
+    Args:
+      unused_argv: Arguments (unused).
+    """
+    logging.set_verbosity(logging.INFO)
+    tf.compat.v1.enable_v2_behavior()
 
-  base_dir = FLAGS.base_dir
-  gin_files, gin_bindings = FLAGS.gin_files, FLAGS.gin_bindings
-  run_experiment.load_gin_configs(gin_files, gin_bindings)
-  # Set the Jax agent seed using the run number
-  create_agent_fn = functools.partial(
-      create_agent, seed=FLAGS.run_number, agent_name=FLAGS.agent
-  )
-  if FLAGS.legacy_runner:
-    runner_fn = eval_run_experiment.MaxEpisodeEvalRunner
-    logging.info('Using MaxEpisodeEvalRunner for evaluation.')
-    runner = runner_fn(base_dir, create_agent_fn)
-  else:
-    runner = atari_100k_runner.DataEfficientAtariRunner(
-        base_dir, create_agent_fn
-    )
-  runner.run_experiment()
+    base_dir = FLAGS.base_dir
+    gin_files, gin_bindings = FLAGS.gin_files, FLAGS.gin_bindings
+    run_experiment.load_gin_configs(gin_files, gin_bindings)
+    # Set the Jax agent seed using the run number
+    create_agent_fn = functools.partial(create_agent, seed=FLAGS.run_number, agent_name=FLAGS.agent)
+    if FLAGS.legacy_runner:
+        runner_fn = eval_run_experiment.MaxEpisodeEvalRunner
+        logging.info("Using MaxEpisodeEvalRunner for evaluation.")
+        runner = runner_fn(base_dir, create_agent_fn)
+    else:
+        runner = atari_100k_runner.DataEfficientAtariRunner(base_dir, create_agent_fn)
+    runner.run_experiment()
 
 
-if __name__ == '__main__':
-  flags.mark_flag_as_required('base_dir')
-  app.run(main)
+if __name__ == "__main__":
+    flags.mark_flag_as_required("base_dir")
+    app.run(main)
--- dopamine/labs/moes/atari_100k_train.py
+++ dopamine/labs/moes/atari_100k_train.py
@@ -28,27 +28,25 @@ from dopamine.labs.moes.agents import rainbow_100k_moe_agent
 import numpy as np
 import tensorflow as tf
 
-.learning.deepmind.xmanager2.client.google as xm  # pylint: disable=unused-import
+# .learning.deepmind.xmanager2.client.google as xm  # pylint: disable=unused-import
 
 
 FLAGS = flags.FLAGS
-AGENTS = ['moe_der']
+AGENTS = ["moe_der"]
 
 # flags are defined when importing run_xm_preprocessing
-flags.DEFINE_enum('agent', 'moe_der', AGENTS, 'Name of the agent.')
-flags.DEFINE_integer('run_number', 1, 'Run number.')
+flags.DEFINE_enum("agent", "moe_der", AGENTS, "Name of the agent.")
+flags.DEFINE_integer("run_number", 1, "Run number.")
+flags.DEFINE_boolean("max_episode_eval", True, "Whether to use `MaxEpisodeEvalRunner` or not.")
 flags.DEFINE_boolean(
-    'max_episode_eval', True, 'Whether to use `MaxEpisodeEvalRunner` or not.'
-)
-flags.DEFINE_boolean(
-    'legacy_runner',
+    "legacy_runner",
     False,  # TODO(psc): Make it work with better runner.
     (
-        'Whether to use the legacy MaxEpisodeEvalRunner.'
-        ' This runner does not run parallel evaluation environments and may be'
-        ' easier to understand, but will be noticeably slower. It also does not'
-        ' guarantee that a precise number of training steps will be collected,'
-        ' which clashes with the technical definition of Atari 100k.'
+        "Whether to use the legacy MaxEpisodeEvalRunner."
+        " This runner does not run parallel evaluation environments and may be"
+        " easier to understand, but will be noticeably slower. It also does not"
+        " guarantee that a precise number of training steps will be collected,"
+        " which clashes with the technical definition of Atari 100k."
     ),
 )
 
@@ -60,68 +58,64 @@ def create_agent(
     agent_name: str,
     summary_writer=None,
 ):
-  """Helper function for creating full rainbow-based Atari 100k agent."""
+    """Helper function for creating full rainbow-based Atari 100k agent."""
 
-  del agent_name
-  return rainbow_100k_moe_agent.Atari100kRainbowMoEAgent(
-      num_actions=environment.action_space.n,
-      seed=seed,
-      summary_writer=summary_writer,
-  )
+    del agent_name
+    return rainbow_100k_moe_agent.Atari100kRainbowMoEAgent(
+        num_actions=environment.action_space.n,
+        seed=seed,
+        summary_writer=summary_writer,
+    )
 
 
 def set_random_seed(seed):
-  """Set random seed for reproducibility."""
-  logging.info('Setting random seed: %d', seed)
-  os.environ['PYTHONHASHSEED'] = str(seed)
-  tf.random.set_seed(seed)
-  np.random.seed(seed)
+    """Set random seed for reproducibility."""
+    logging.info("Setting random seed: %d", seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    tf.random.set_seed(seed)
+    np.random.seed(seed)
 
 
 def main(unused_argv):
-  """Main method.
+    """Main method.
 
-  Args:
-    unused_argv: Arguments (unused).
-  """
-  logging.set_verbosity(logging.INFO)
-  tf.compat.v1.enable_v2_behavior()
+    Args:
+      unused_argv: Arguments (unused).
+    """
+    logging.set_verbosity(logging.INFO)
+    tf.compat.v1.enable_v2_behavior()
 
-  base_dir = FLAGS.base_dir
-  gin_files, gin_bindings = FLAGS.gin_files, FLAGS.gin_bindings
-  xm_xid = None if 'xm_xid' not in FLAGS else FLAGS.xm_xid
-  xm_wid = None if 'xm_wid' not in FLAGS else FLAGS.xm_wid
-  xm_parameters = None if 'xm_parameters' not in FLAGS else FLAGS.xm_parameters
-  if xm_parameters:
-    xm_params = json.loads(xm_parameters)
-    if 'run_number' in xm_params:
-      FLAGS.run_number = xm_params['run_number']
-      logging.info('Run number set to: %d', xm_params['run_number'])
-  # Add code for setting random seed using the run_number
-  set_random_seed(FLAGS.run_number)
-  base_dir, gin_files, gin_bindings = base_train.run_xm_preprocessing(
-      xm_xid,
-      xm_wid,
-      xm_parameters,
-      FLAGS.base_dir,
-      FLAGS.custom_base_dir_from_hparams,
-      FLAGS.gin_files,
-      FLAGS.gin_bindings,
-  )
-  run_experiment.load_gin_configs(gin_files, gin_bindings)
-  # Set the Jax agent seed using the run number
-  create_agent_fn = functools.partial(
-      create_agent, seed=FLAGS.run_number, agent_name=FLAGS.agent
-  )
-  if FLAGS.legacy_runner:
-    runner = run_experiment.Runner(base_dir, create_agent_fn)
-  else:
-    runner = atari_100k_runner.DataEfficientAtariRunner(
-        base_dir, create_agent_fn
+    base_dir = FLAGS.base_dir
+    gin_files, gin_bindings = FLAGS.gin_files, FLAGS.gin_bindings
+    xm_xid = None if "xm_xid" not in FLAGS else FLAGS.xm_xid
+    xm_wid = None if "xm_wid" not in FLAGS else FLAGS.xm_wid
+    xm_parameters = None if "xm_parameters" not in FLAGS else FLAGS.xm_parameters
+    if xm_parameters:
+        xm_params = json.loads(xm_parameters)
+        if "run_number" in xm_params:
+            FLAGS.run_number = xm_params["run_number"]
+            logging.info("Run number set to: %d", xm_params["run_number"])
+    # Add code for setting random seed using the run_number
+    set_random_seed(FLAGS.run_number)
+    base_dir, gin_files, gin_bindings = base_train.run_xm_preprocessing(
+        xm_xid,
+        xm_wid,
+        xm_parameters,
+        FLAGS.base_dir,
+        FLAGS.custom_base_dir_from_hparams,
+        FLAGS.gin_files,
+        FLAGS.gin_bindings,
     )
-  runner.run_experiment()
+    run_experiment.load_gin_configs(gin_files, gin_bindings)
+    # Set the Jax agent seed using the run number
+    create_agent_fn = functools.partial(create_agent, seed=FLAGS.run_number, agent_name=FLAGS.agent)
+    if FLAGS.legacy_runner:
+        runner = run_experiment.Runner(base_dir, create_agent_fn)
+    else:
+        runner = atari_100k_runner.DataEfficientAtariRunner(base_dir, create_agent_fn)
+    runner.run_experiment()
 
 
-if __name__ == '__main__':
-  flags.mark_flag_as_required('base_dir')
-  app.run(main)
+if __name__ == "__main__":
+    flags.mark_flag_as_required("base_dir")
+    app.run(main)
